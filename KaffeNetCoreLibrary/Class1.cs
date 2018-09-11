e8ac452c38072aea065ab9d9d834856379471c00
Martin Kierkegaard <mark@easj.dk>
2018-09-11 20:26:20 +0200

    .net core library som indeholder kaffe klasser

--- KaffeNetCoreLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace KaffeNetCoreLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ KaffeNetCoreLibrary/kaffe/FlatWhite.cs
@@ -0,0 +1,26 @@
+﻿using kaffe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaffeNetCore.kaffe
+{
+    public class FlatWhite : Kaffe , Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+    }
+}
